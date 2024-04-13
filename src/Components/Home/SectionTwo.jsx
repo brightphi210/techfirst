7df1e87e855694bf6455db132f011c32e28d2a62
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-04-13 08:30:22 -0700

    a

--- src/Components/Home/SectionTwo.jsx
+++ src/Components/Home/SectionTwo.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import bigImage from '../images/mg.jpg'
+import { FaLocationArrow } from "react-icons/fa6";
 
 const SectionTwo = () => {
   return (
-    <div>SectionTwo</div>
+    <div className='sectionTwoMain'>
+        <div className='sectionDiv'>
+          <div className='sectionTwoText'>
+            <h2>The biggest Port Harcourt <br /> Tech event yet</h2>
+
+            <p>
+              Experience Canva Create in person with 50+ speakers, 15+ sessions and workshops led by the Canva team. Can’t make it to L.A.? Tune in to the keynote online to get a first look at the new launches.
+            </p>
+
+            <button>Get free ticktet <FaLocationArrow /></button>
+          </div>
+
+          <div className='sectionTwoImgDiv'>
+              <img src={bigImage} alt="" />
+          </div>
+        </div>
+    </div>
   )
 }
 
